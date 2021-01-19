bda3002c1acdf6872c1492438709c8bb40ae3b90
Kbadr1 <kbadr.dev@gmail.com>
2021-01-19 02:33:14 +0200

    footer responsiveness edited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,37 +3,45 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="Footer container">
-      <div className="row">
-        <div className="logo col-lg-3">
-          <h2>LOGO</h2>
-          <p>&copy; 2021 Chefaa. All Rights Reserved</p>
-        </div>
-        <div className="col-lg-3">
-          <a href="">About Chefaa</a>
-          <br />
-          <a href="">Blog</a>
-          <br />
-          <a href="">Are You A Pharmacy Owner?</a>
-        </div>
-        <div className="col-lg-3">
-          <a href="">Privacy Policy</a>
-          <br />
-          <a href="">Contact Us</a>
-          <br />
-          <a href="">Terms & Conditions</a>
-        </div>
-        <div className="follow col-lg-3">
-          <p>Follow Us</p>
-          <div className="row">
-            <div className="col-4">
-              <i class="fab fa-instagram-square"></i>
-            </div>
-            <div className="col-4">
-              <i class="fab fa-twitter-square"></i>
-            </div>
-            <div className="col-4">
-              <i class="fab fa-facebook-square"></i>
+    <div className="Footer ">
+      <div className="container">
+        <div className="row">
+          <div className="logo col-12 col-md-3">
+            <h2>LOGO</h2>
+            <p>&copy; 2021 Chefaa. All Rights Reserved</p>
+          </div>
+          <div className="col-6 col-md-3">
+            <a href="">About Chefaa</a>
+            <br />
+            <a href="">Blog</a>
+            <br />
+            <a href="">Are You A Pharmacy Owner?</a>
+          </div>
+          <div className=" col-6 col-md-3">
+            <a href="">Privacy Policy</a>
+            <br />
+            <a href="">Contact Us</a>
+            <br />
+            <a href="">Terms & Conditions</a>
+          </div>
+          <div className="follow col-12 col-md-3">
+            <p>Follow Us</p>
+            <div className="icons row">
+              <div className="col-4">
+                <a href="">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </div>
+              <div className="col-4">
+                <a href="">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </div>
+              <div className="col-4">
+                <a href="">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
