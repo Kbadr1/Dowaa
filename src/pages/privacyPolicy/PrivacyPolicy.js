d9619dafde49748b4d770d85506eefa4b5ed6654
Kbadr1 <kbadr.dev@gmail.com>
2021-01-19 10:53:46 +0200

    added privacyPolicy page

---
+++ src/pages/privacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,298 @@
+import React from "react";
+import "./privacyPolicy.css";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="PrivacyPolicy container">
+      <h2>Privacy Policy</h2>
+      <div className="content">
+        <div className="box">
+          <h4>Privacy policy at CHEFAA</h4>
+          <p>
+            Chefaa application and the associated website www.Chefaa.Com is
+            owned and operated by chefaa, and the chefaa application and website
+            provide a platform through which you can order prescriptions and
+            non-pharmaceuticals
+          </p>
+          <p>
+            Chefaa’s privacy policy aims to inform you of our policies and
+            practices regarding information collection, use, and disclosure of
+            any information provided to us or collected through your use of the
+            chefaa app and/or website.
+          </p>
+          <p>
+            The information covered in the privacy policy includes “personal
+            information”, which is information about you that can identify you
+            like your name, your real address, or an email address, as well as
+            “non-identifiable information
+          </p>
+        </div>
+        <div className="box">
+          <h4>User satisfaction</h4>
+          <p>
+            By accessing or using chefaa, you agree to the terms and conditions
+            of the privacy policy and the associated terms of service, and that
+            you explicitly agree to the processing of your personal and
+            non-identifying information in accordance with this privacy policy.
+          </p>
+          <p>
+            Your personal information may be processed by us in the country in
+            which it is collected.
+          </p>
+        </div>
+        <h3>Collection and use of information</h3>
+        <div className="box">
+          <h4>Information provided to chefaa</h4>
+          <p>
+            We collect both personal and non-identifiable information that you
+            voluntarily send to us through chefaa during the process of creating
+            or modifying your account and user profile on chefaa. For example,
+            our registration and login process requires you to provide us with
+            your name, correct email address, and password of your choice, and
+            we also collect your mobile phone number as part of your
+            registration information, and when you edit your profile and use
+            chefaa benefits we will collect any information you provide
+            voluntarily, and we may also request optional information to support
+            your use of shifa, including such equipment non-identifiable
+            information, and we also collect information in the form of content
+            that you provide during your use of chefaa, such as reports,
+            assessments and other information that you choose to provide,
+            including information from other social networks that you choose to
+            link to your profile at chefaa.
+          </p>
+          <p>
+            If you send an email to us, or fill out the “comments - contact us”
+            form through chefaa, we will collect your email address and the full
+            content of your email, including the attached files, and other
+            information you provide, and when completing the financial
+            transactions with chefaa. You will need to provide your credit card
+            or other payment information so that we can process your payments
+            for those products and services, and when participating in contests,
+            or other activities that we provide on chefaa, we will collect
+            specific information along with such activity, which may include
+            your personal information to allow us to contact you regarding this
+            activity.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Information from other sources</h4>
+          <p>
+            We may take your personal information and other information from
+            other sources that you have registered with, or companies with which
+            we have a partnership or other third parties, and we may link this
+            information with other personal information that we have collected
+            about you, and if you contact chefaa through another social network,
+            we may collect information about you and your friends from this
+            social network in order to offer you a more personalized experience,
+            and chefaa will process this information in the same way that we
+            deal with the information we collect from you directly, however, you
+            must remember that the providers of such information from the third
+            party deal with it according to the policies particular of their
+            own.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Information collected through technological means</h4>
+          <p>
+            As you use chefaa, some technical and usage information may be
+            collected from you, including your internet protocol address,
+            browser type, and operating system. Regarding your use of chefaa
+            application from a mobile device, information may also be collected
+            from your mobile device, including its unique identification number
+            and your geographic location, and we also use cookies and browsing
+            data such as unified uniform resource locators (url) to collect
+            information about the date and time of your visit and the solutions
+            and information that you have searched for and viewed, or any of the
+            displayed ads that you have clicked on chefaa website and we collect
+            this type of information to make chefaa and its solutions more
+            useful to you.
+          </p>
+          <p>
+            In order for you to use the services we provide through chefaa, your
+            web browser must accept cookies. If you choose to disable cookies,
+            chefaa may not function properly and you will not be able to obtain
+            our services.
+          </p>
+          <p>
+            We may also enable third-party advertising networks, advertisers,
+            and advertising services to promote third-party products and / or ad
+            network servers by way of advertisements, and these advertisers and
+            ad network servers may use their cookies to monitor information
+            related to those ads and your use of chefaa and when you click on
+            these advertisements you will be directed to the third-party website
+            and advertiser, and this privacy policy does not include the privacy
+            practices of any of the advertisers or ad network servers.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Aggregate information</h4>
+          <p>
+            In addition, we may use your non-identifiable information in
+            relation to collecting aggregate information about the general use
+            of the website in order to perform statistical analysis to assist us
+            in analyzing patterns in the use of chefaa and such aggregate
+            information will not include your personal information.
+          </p>
+        </div>
+        <h3>Use and disclosure of information</h3>
+        <div className="box">
+          <h4>Use of information in chefaa</h4>
+          <p>
+            In general, both personal and non-identifiable information that you
+            send us are used to enable you to gain access to chefaa and to
+            improve chefaa.{" "}
+          </p>
+          <p>
+            Additionally, we may use your email address to contact you for
+            things such as notifications of changes in chefaa policies and other
+            types of services and / or legal notices, and your preference may
+            appear to stop receiving more promotional messages.{" "}
+          </p>
+        </div>
+        <div className="box">
+          <h4>Disclosure of information in chefaa</h4>
+          <p>
+            Personal information: except as otherwise provided in this privacy
+            policy, we generally do not sell, trade, rent, or exchange personal
+            information that we collect with third parties, unless you request
+            or authorize us to do so.
+          </p>
+          <p>
+            Partners and service providers: we share your preferences for
+            content and other information with the social network through which
+            you communicate with chefaa alongside those companies and persons
+            with whom you have requested or authorized us to share your
+            information.
+          </p>
+          <p>
+            We can provide your personal information to third-party service
+            providers who work on our behalf or with us to provide some services
+            and benefits from chefaa and to help us communicate with you
+          </p>
+          <p>
+            Non-identifiable information: this privacy policy does not limit our
+            use of non-identifiable information or disclosure in any way, and we
+            reserve the right to use and disclose non-identifiable information
+            to our partners, advertisers, and other third parties at our
+            discretion, and in particular, we may use any of the
+            non-identifiable information that you provide or that we collect
+            from you or third parties to help us improve content or
+            communications, and we may disclose non-identifiable information to
+            third parties who help us in providing you with the most important
+            advertisements and content.
+          </p>
+          <p>
+            In the event of a commercial transition such as a merger,
+            acquisition by another company, or sale of all or part of its
+            assets, your personal information may be among the transferred
+            assets, and you acknowledge that such transfers may occur and are
+            permitted in accordance with this privacy policy, and that any
+            holders of us or their agents may continue to process your personal
+            information as stipulated in this privacy policy.
+          </p>
+          <p>
+            Responding to legal procedures and related disclosures: we may
+            disclose your personal information if we believe in good faith that
+            such disclosure is necessary{" "}
+          </p>
+          <p>
+            (a) comply with laws or respond to subpoenas, court orders, orders
+            or other legal claims, or to bring a case or defend chefaa from
+            legal claims
+          </p>
+          <p>
+            (b) to protect and defend our rights and property, or on your
+            behalf, or third parties from fraud, abuse, or abusive treatment,
+            violations of conditions of service or wrongful acts
+          </p>
+          <p>
+            (c) to protect the safety or integrity of chefaa, the users, the
+            public or other parties at our discretion, and you hereby consent to
+            our sharing of your personal information under the circumstances
+            referred to in this document.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Third party websites and ads</h4>
+          <p>
+            Chefaa may contain links to other websites, so please be aware that
+            we are not responsible for the privacy practices or the content of
+            such third-party sites, and we encourage our users to read the
+            privacy statements of each site they visit, and this privacy applies
+            only to information collected by us through chefaa and does not
+            apply to these third-party websites, and the ability to access
+            third-party information from chefaa or links to other sites or
+            places is for your convenience and does not imply our endorsement of
+            such third parties, their products, services, other sites, or places
+            or content.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Your choices regarding your personal information</h4>
+          <p>
+            We offer you options related to collecting, using, and exchanging
+            your personal information. When you receive promotional messages
+            from us, you can indicate your preference to stop receiving more
+            promotional messages from us and you will have the opportunity to
+            “opt out” by following the unsubscribe instructions provided in the
+            promotional email or by direct contact with us.
+          </p>
+          <p>
+            Despite your indicated email preferences, we can also send you
+            administrative e-mail messages related to chefaa including, for
+            example, administrative confirmations, and notifications of updates
+            to this privacy policy if we choose to provide such feedback to you
+            in this manner.
+          </p>
+          <p>
+            You can change any of your profile information by editing it on the
+            profile settings page, and you can request to delete your personal
+            information by contacting us at info@chefaa.Com , but please note
+            that we may need (legally or otherwise) to maintain this information
+            and not delete it (or to keep this information for a certain period
+            and in this case we will adhere to your request for deletion, only
+            after we have fulfilled such conditions), and when we delete
+            personal information, it will be deleted from the active database,
+            but it may remain in our archive.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Comments</h4>
+          <p>
+            If you make any comments to us, we may use and disclose such
+            opinions for any purpose, provided that we do not link such notes
+            with your personal information, and we will collect any information
+            contained in these comments and the personal information contained
+            therein will be processed according to this privacy policy.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Information security</h4>
+          <p>
+            We are committed to protecting the security of your personal
+            information, but no company, including us, can completely eliminate
+            the security risks of personal information.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Contact and reviews</h4>
+          <p>
+            If you have questions or concerns about our privacy policy, please
+            contact us at info@chefaa.Com.
+          </p>
+          <p>
+            This privacy policy is subject to review from time to time at our
+            discretion. If we make any substantial changes to the way we use
+            your personal information, we will post the notice on this page. If
+            you object to any of these changes, you should stop using chefaa. As
+            for your continued use of chefaa following a notice for any such
+            changes, this indicates your acknowledgment of such changes and an
+            agreement to adhere to the terms and conditions of such changes.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
