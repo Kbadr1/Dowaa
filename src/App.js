8316e67e9b8b042da07081a0c98a4741ce9810fb
Kbadr1 <kbadr.dev@gmail.com>
2021-01-19 11:11:37 +0200

    added termsOfService page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import PharmacyOwner from "./pages/pharmacyOwner/PharmacyOwner";
 import PrivacyPolicy from "./pages/privacyPolicy/PrivacyPolicy";
+import TermsOfService from "./pages/termsOfService/TermsOfService";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <PrivacyPolicy />
+      <TermsOfService />
       <Footer />
     </div>
   );
---
+++ src/pages/termsOfService/TermsOfService.js
@@ -0,0 +1,300 @@
+import React from "react";
+import "./termsOfService.css";
+
+const TermsOfService = () => {
+  return (
+    <div className="TermsOfService container">
+      <h2>Privacy Policy</h2>
+      <div className="content">
+        <div className="box">
+          <h4>Terms of service at CHEFAA</h4>
+          <p>
+            The following terms and conditions represent a binding agreement
+            between you and (Chefaa), so we hope you read the following
+            conditions carefully, when you use mobile applications or the
+            website, this means that you agree to these terms and conditions
+            that govern your use of applications or the website, and in the
+            event that you do not agree to these terms , You must stop the use
+            of applications and the website, and the phrases “Chefaa” or “we”
+            and their meanings refer to Chefaa application; the owner and
+            operator of the application and the site, as the phrase site,
+            website, application or applications refer to the services provided
+            by Chefaa, while the phrase ” you ” refers to the person who
+            installs or uses the application, or the entity represented by the
+            person acting.
+          </p>
+        </div>
+        <div className="box">
+          <h4>
+            You are allowed to use the Chefaa Application in the following
+            cases:
+          </h4>
+          <p>You can form a binding contract in your jurisdiction.</p>
+          <p>Compliance with terms and conditions.</p>
+          <p>
+            Give accurate and complete information when creating a user account.
+          </p>
+          <p>
+            Acknowledgment that you are solely responsible for using the
+            application when logging in to Chefaa.
+          </p>
+          <p>
+            Not to collect any personal information for commercial purposes
+            including full name, address and e-mail.
+          </p>
+          <p>
+            Acknowledgment that you are solely responsible for providing your
+            content, including conversations, profile information, links,
+            images, other content,etc.,
+          </p>
+          <p>
+            Confirm that you have the necessary licenses, rights, powers, and
+            approvals to use and authorize Chefaa to use all trademarks, trade
+            secrets, copyrights or other property rights in all entries to
+            enable inclusion and use through Chefaa application.
+          </p>
+          <p>
+            Granting each user of Chefaa, whether using Chefaa application or
+            any application developed by a third party, a non-exclusive license
+            to access your inputs through Chefaa, use, reproduce, distribute and
+            prepare products derived from it, display and execute such requests
+            as authorized by Chefaa application and under these terms of
+            service.
+          </p>
+          <p>
+            You will not provide copyrighted material or subject to third party
+            property rights, including privacy, publicity, commercial secrets,
+            etc., unless you own these rights or have the appropriate permission
+            from their rightful owner to provide those materials for recovery.
+          </p>
+          <p>
+            Confirm that we have the right to determine whether any of your
+            entries are appropriate and in accordance with our terms of service,
+            remove any or all of them, and terminate your account with or
+            without prior notice.{" "}
+          </p>
+        </div>
+        <div className="box">
+          <h4>Your account and profile on Chefaa</h4>
+          <p>
+            You will need to register by creating an account with us, and when
+            creating an account or personal file on Chefaa application, you
+            agree to provide the registration information accurately and
+            completely, and you will update that information if necessary, and
+            that access to the application is not authorized by any person or
+            other party . By using your registration information you are
+            responsible for preventing such unauthorized use.
+          </p>
+          <p>
+            After registering, we will issue you a special account number with
+            your phone number or email address (user ID). As for individuals and
+            entities who access the application or use the services and their
+            accounts have been previously terminated by Chefaa, they are not
+            allowed to register for an account, and you may not allow any of
+            these People use your account on your behalf.
+          </p>
+          <p>
+            Chefaa application relies on user IDs to determine if users of the
+            application are allowed to do so, and if someone accesses the
+            application using the user ID that we have issued to you, we will
+            rely on this user ID, and we will assume that it really is you or
+            your representative to use the application, so You are solely
+            responsible for any access to the application or use of the service
+            by anyone who uses your username, so please inform us immediately if
+            you know that your username is used without a license
+          </p>
+        </div>
+        <div className="box">
+          <h4>Rules for information and other content</h4>
+          <p>
+            When you use the application, you can publish and obtain access to
+            different types of information and materials, which we call
+            “content”, and the content also includes information and materials
+            published on the application by you, so you agree not to modify the
+            content published by others, as well as agree not to Posting or
+            using any content in any form that violates the laws of the market,
+            including, without limitation, the following:
+          </p>
+          <p>
+            Violation of the copyrights, trademarks, trade secrets, or other
+            intellectual property, or the property rights of others.
+          </p>
+          <p>
+            Violation of privacy, publicity, or any rights of third parties.
+          </p>
+          <p>
+            All that is unlawful, slanderous, racial discrimination, defamation,
+            pornography, immorality, abuse, threat, harassment or encouragement
+            of behavior that would be considered a criminal offense, leading to
+            civil liability, violation of regulations, or anything inappropriate
+            as determined by Chefaa at our discretion.
+          </p>
+          <p>Everything is false or inaccurate.</p>
+          <p>
+            All that could harm our company, parent company, sister companies,
+            affiliates, advertisers, or other parties.
+          </p>
+          <p>
+            Although we strive to implement these rules with all of our users,
+            we may be exposed through the application or services to content
+            that violates our policies or on the other hand an attack on the
+            content, so the use of applications and services is at your own
+            risk, and although we are not obligated, only We may terminate the
+            accounts of these users and remove the content from the application
+            if we find or suspect that those accounts or the content violates
+            the terms of this agreement or the applicable agreement with the
+            violating user, without taking any responsibility for the exposure
+            of the content of the application whether it violates our content
+            policies or Not.
+          </p>
+          <p>
+            If you provide comments to us, we may use and disclose such opinions
+            for any purpose, according to these terms of service and privacy
+            policy, you agree that any of these notes and any email we receive,
+            becomes ours, and we may use your notes for marketing purposes or to
+            add or amend our services without paying you concession fees or any
+            other compensation.{" "}
+          </p>
+        </div>
+        <div className="box">
+          <h4>Confirm the purchase order</h4>
+          <p>
+            All requests received, will be confirmed and accepted through our
+            Chefaa app or website.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Amendments and cancellations</h4>
+          <p>
+            We can accept modifications to orders by phone, website or
+            application, provided the order is not out of stock or shipped. We
+            will ensure that the request is subject to change as possible. Under
+            any circumstances, and when there is not enough product required, we
+            will strive to inform you in due time.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Delivery :</h4>
+          <p>
+            Chefaa is happy to serve you all days of the week. The cost of
+            delivery service may vary from one pharmacy to another.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Payment method :</h4>
+          <p>
+            Payment is made either Cash on delivery (COD) or by charging the
+            wallet.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Please note that:</h4>
+          <p>
+            After charging the wallet, final cost of your order is automatically
+            deducted from wallet, If the amount in the wallet is not sufficient,
+            its available balance will be used and then you can pay the rest in
+            cash After charging the wallet, balance shall be used on your order
+            and can’t be transferred back to your credit card.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Products fulfilled by Sellers:</h4>
+          <p>
+            Chefaa is not an auction house nor a bank, and does not offer
+            auction or banking services. Chefaa is an online platform allowing
+            for the sale and purchase of items between end-user sellers and
+            buyers.
+          </p>
+          <p>
+            By providing the Wallet and COD payment facilities, Chefaa is merely
+            facilitating the making of online payments by buyers possible but
+            Chefaa is not involved in the process of buying and selling of such
+            items on the platform.
+          </p>
+          <p>
+            All sales and purchases on the platform continue to be bipartite
+            contracts between the buyer and the seller of an item(s) and chefaa
+            is not responsible for any non-performance, breach or any other
+            claim relating to or arising out of any contract entered into
+            between any buyers and sellers, nor does chefaa have any fiduciary
+            duty to any user. You acknowledge and agree that chefaa will hold
+            your funds at your own risk.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Providing Feedback:</h4>
+          <p>
+            Chefaa encourages users on the platform to provide feedback/rating
+            after a transaction is closed.
+          </p>
+          <p>
+            You agree not to make comments that are not factual in nature
+            including without limitation making any racist comments, using
+            profanity, abusing another user, disrespecting another culture or
+            any other derogatory or inappropriate comments.
+          </p>
+          <p>
+            You further agree not to post comments in order to solicit sales
+            outside of the platform and not to display contact information of
+            any person within any feedback.
+          </p>
+          <p>
+            And Chefaa reserves the right to suspend, limit or withdraw your
+            access to the platform.
+          </p>
+        </div>
+        <div className="box">
+          <h4>General</h4>
+          <p>
+            If any clause of this User Agreement shall be deemed invalid, void
+            or for any reason unenforceable, such clause shall be deemed
+            severable and shall not affect the validity and enforceability of
+            the remaining clauses of this User Agreement.
+          </p>
+          <p>
+            This User Agreement (as amended from time to time in accordance with
+            the terms of this User Agreement) sets forth the entire
+            understanding and agreement between you and Chefaa with respect to
+            the subject matter hereof.
+          </p>
+        </div>
+        <div className="box">
+          <h4>Refund Requests (Returned):</h4>
+          <p>
+            If you change your mind after making the order, all you have to do
+            is contact us to cancel your order, and we will return any sums that
+            were paid in the event of online payment. The customer shall bear
+            any additional transfer fees.
+          </p>
+          <p>
+            If you want to return the product or medicine after receiving it, it
+            cannot be returned except in the event that it does not meet the
+            specifications in the original order sent. Chefaa does not support
+            the replacement of the product.
+          </p>
+          <p>
+            In the event that the product does not meet the specifications, the
+            product will be returned without the customer incurring the shipping
+            fees
+          </p>
+          <p>
+            In the event of return and if the payment method is cash on
+            delivery, the customer has the right to refund the amount in cash
+            within a maximum period of 10 days.
+          </p>
+          <p>
+            In the event of return and if payment method was through the wallet,
+            the wallet will be recharged with the order value and not cash in
+            hand.
+          </p>
+          <p>
+            Chefaa reserves the right to cancel the request in the event that
+            the customer does not adhere to the fair use policy.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TermsOfService;
