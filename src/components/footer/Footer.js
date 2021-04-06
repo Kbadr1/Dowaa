1a6a154872dc799c8a3c27aafe6c52562adb4401
Kbadr1 <kbadr.dev@gmail.com>
2021-04-06 14:20:24 +0200

    fixed footer links and logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import logo from "../../styles/Capture.PNG";
 import "./footer.scss";
 
 const Footer = () => {
@@ -8,38 +9,36 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="logo col-12 col-md-3">
-            <h2>LOGO</h2>
-            <p>&copy; 2021 Chefaa. All Rights Reserved</p>
+            <img src={logo} alt="" />
+            <p>&copy; 2021 Dowaa. All Rights Reserved</p>
           </div>
           <div className="col-6 col-md-3">
-            <Link to="/about-us">About Chefaa</Link>
-            <br />
-            <Link to="">Blog</Link>
-            <br />
-            <Link to="/pharmacy-owner">Are You A Pharmacy Owner?</Link>
-          </div>
-          <div className=" col-6 col-md-3">
-            <Link to="/privacy-policy">Privacy Policy</Link>
-            <br />
-            <Link to="/contact-us">Contact Us</Link>
+            <Link to="/about-us">About Dowaa</Link>
             <br />
             <Link to="/terms-service">Terms & Conditions</Link>
+            <br />
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </div>
+          <div className=" col-6 col-md-3">
+            <Link to="/contact-us">Contact Us</Link>
+            <br />
+            <Link to="/pharmacy-owner">Are You A Pharmacy Owner?</Link>
           </div>
           <div className="follow col-12 col-md-3">
             <p>Follow Us</p>
             <div className="icons row">
               <div className="col-4">
-                <a href="">
+                <a href="https://www.instagram.com" target="_blank">
                   <i class="fab fa-instagram"></i>
                 </a>
               </div>
               <div className="col-4">
-                <a href="">
+                <a href="https://twitter.com/" target="_blank">
                   <i class="fab fa-twitter"></i>
                 </a>
               </div>
               <div className="col-4">
-                <a href="">
+                <a href="https://www.facebook.com" target="_blank">
                   <i class="fab fa-facebook-f"></i>
                 </a>
               </div>
